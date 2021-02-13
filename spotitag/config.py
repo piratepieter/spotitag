75e87ea2ebe017c6bc85a4f3b48dc6aead2b30dc
Pieter Vancraeyveld <pieter.vancraeyveld@gmail.com>
2021-02-13 16:53:52 +0000

    Define flask secret via environment variable

--- spotitag/config.py
+++ spotitag/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config(object):
-    SECRET_KEY = 'omg'
+    SECRET_KEY = os.environ['SPOTITAG_SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['SPOTITAG_URI']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
