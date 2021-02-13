aa923f3543c7b8421f453f38eba3bf315c65602a
Pieter Vancraeyveld <pieter.vancraeyveld@gmail.com>
2021-02-13 17:12:58 +0000

    Define tags table

--- spotitag/models.py
+++ spotitag/models.py
@@ -14,6 +14,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    tags = db.relationship('Tag', backref='user', lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -23,6 +24,16 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return self.username
+
+
+class Tag(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(120), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return self.label
 
 
 @login.user_loader
