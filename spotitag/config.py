eeafcd58de6a08b2971922ba8aa98c437ce3357f
Pieter Vancraeyveld <pieter.vancraeyveld@gmail.co>
2021-02-20 11:01:55 +0100

    Rename env variable for database to match heroku addon

--- spotitag/config.py
+++ spotitag/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ['SPOTITAG_SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = os.environ['SPOTITAG_URI']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
