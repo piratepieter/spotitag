5fd82d4bf97fb30ba1d61f8489d03f22baa10898
Pieter Vancraeyveld <pieter.vancraeyveld@gmail.co>
2021-02-20 10:50:19 +0100

    app for gunicorn

---
+++ app.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+load_dotenv('.env')
+
+from spotitag import app, db
+from spotitag.models import Artist, Tag, User
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Tag': Tag, 'Artist': Artist}
